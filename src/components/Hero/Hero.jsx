9a1befc4c049f84f9c57b0d3b61472cf89447e4a
abutalhasaifi <abutalha.mca21@cs.du.ac.in>
2024-07-07 20:32:38 +0530

    Add .gitignore file

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Abutalha</h1>
+        <p className={styles.description}>
+          I am currently pursuing MCA from the Department of Computer Science, University of Delhi (DUCS).
+          <br />
+          I'm a Full-Stack Developer with strong Knowledge of both Front-End and Back-End.
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/ImageHero.jpg")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
